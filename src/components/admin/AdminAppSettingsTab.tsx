fc03373ab3d80c1a4de79009b56c908db71efb19
Michael Jurisch <mj@diffraktion.de>
2026-02-24 08:47:39 +0100

    feat: add column reordering with up/down arrows in default board columns

--- src/components/admin/AdminAppSettingsTab.tsx
+++ src/components/admin/AdminAppSettingsTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import AdminFileUploadsTab from './AdminFileUploadsTab';
 import AdminNotificationQueueTab from './AdminNotificationQueueTab';
